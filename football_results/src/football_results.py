060eec443decaa3e7a60f4d6907dce8ea00aca74
Jamie <jamiefogartykk@gmail.com>
2022-06-30 13:47:00 +0100

    second commit

--- football_results/src/football_results.py
+++ football_results/src/football_results.py
@@ -1,6 +1,12 @@
 def get_result(final_score):
+    final_score +
+    if final_score >= 
+    return "Home Win"
+    return "Away Win"
+    return "Draw"
     pass
-
+"home_score": 0,
+    "away_score": 1
 def get_results(final_scores):
     pass
     # (You could try and use a list comprehension for this)
--- football_results/tests/football_results_test.py
+++ football_results/tests/football_results_test.py
@@ -2,7 +2,10 @@ import unittest
 from src.football_results import *
 
 class FootballResultsTest(unittest.TestCase):
-    pass
+    def test_get_result_home_win(self):
+        result = {"home_score": 1, "away_score": 0}
+        self.assertEqual("Home win", self.football_results.get_result(result))
+
     # Test we get the right result string for a final score dictionary representing -
 
         # Home win
